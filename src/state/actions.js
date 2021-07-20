087157a4b00a2f4e82065e3618936396bbdf11d2
Duc Ngo Viet <duc@talentwunder.com>
2021-07-20 17:08:29 +0200

    cleanup and add basic structure

---
+++ src/state/actions.js
@@ -0,0 +1,10 @@
+// Find Hacker News API here
+// https://github.com/HackerNews/API
+
+// TODO fetch ask stories (https://hacker-news.firebaseio.com/v0/askstories.json)
+export function loadTopstories() {
+    return async (dispatch, getState) => {};
+}
+
+// TODO fetch item by id (https://hacker-news.firebaseio.com/v0/item/<itemId>.json)
+function fetchStoryById(id) {}
