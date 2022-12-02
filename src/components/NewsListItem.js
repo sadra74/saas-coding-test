553c538265d24796bb74e82b0302c7f748a4d88d
sadra <sadra.74@live.com>
2022-12-02 12:56:50 +0100

    removing href

--- src/components/NewsListItem.js
+++ src/components/NewsListItem.js
@@ -30,11 +30,11 @@ export function NewsListItem({id}) {
         {!loading ? (!error ? (<div onClick={() => getKids() ? setOpen(!open) : null}
                                     className="item-container">
             <Icon kids={getKids()} open={open}/>
-            <a href={item?.url} className="item">
+            <div className="item">
                 <p className="id">{item?.id}</p>
                 <h4 className="title">{item?.title}</h4>
                 <p className="text">{item?.text?.substring(0,50) + "..."}</p>
-            </a>
+            </div>
         </div>) : "An error occurred") : "..."}
         {open && <NewsList ids={getKids() ?? []}></NewsList>}
     </>
